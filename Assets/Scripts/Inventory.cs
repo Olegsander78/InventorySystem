1a797f15a3d9d3f8202cf1f2a51526a8423a68ff
Olegsander78 <onkotov@gmail.com>
2022-08-24 09:00:46 +0500

    Сделано - написаны основные скрипты инвентаря

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> characterItems = new List<Item>();
+    public ItemDatabase itemDatabase;
+    public UIInventory inventoryUI;
+
+
+    private void Start()
+    {
+        GiveItem(1);
+    }
+    public void GiveItem(int id)
+    {
+        Item itemAdd = itemDatabase.GetItem(id);
+        characterItems.Add(itemAdd);
+        inventoryUI.Addnewitem(itemAdd);
+        Debug.Log("Added item: " + itemAdd.title);
+    }
+    public void GiveItem(string itemName)
+    {
+        Item itemAdd = itemDatabase.GetItem(itemName);
+        characterItems.Add(itemAdd);
+        Debug.Log("Added item: " + itemAdd.title);
+    }
+    public Item CheckForItem(int id)
+    {
+        return characterItems.Find(item => item.id == id);
+    }
+    public void RemoveItem(int id)
+    {
+        Item itemToRemove = CheckForItem(id);
+        if(itemToRemove != null)
+        {
+            characterItems.Remove(itemToRemove);
+            inventoryUI.RemoveItem(itemToRemove);
+        }
+    }
+}
